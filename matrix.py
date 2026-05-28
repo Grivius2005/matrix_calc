a6ff3bcc1c8176249ccac09fe5dafeb57f37e11e
Agata Zurek <agatazurek@student.agh.edu.pl>
2026-05-29 00:29:16 +0200

    dodano funkcję sprawdzającą macierze pod względem mnożenia macierzy

--- matrix.py
+++ matrix.py
@@ -1,1 +1,6 @@
 #klasa  Matrix :)
+
+class Matrix:
+
+    def is_it_ok_to_multiply(self, matrix2: Matrix) -> bool:
+        return len(self.matrix[0]) == len(matrix2.matrix) #dostosować nazwę pola matrix
