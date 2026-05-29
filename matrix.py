162c5ce69cf6275b2c8db2abf1ce2635cbe43176
Agata Zurek <agatazurek@student.agh.edu.pl>
2026-05-30 01:50:02 +0200

    dodano funkcję wybierającą metodę potęgowania macierzy

--- matrix.py
+++ matrix.py
@@ -4,3 +4,7 @@ class Matrix:
 
     def is_it_ok_to_multiply(self, matrix2: Matrix) -> bool:
         return len(self.matrix[0]) == len(matrix2.matrix) #dostosować nazwę pola matrix
+
+    def which_power_method(self, method: string) -> int:
+        dict = {"Jordan" : 1, "multiply" : 2}
+        return dict[method] #to nie ma sensu
