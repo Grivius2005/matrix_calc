ec7bacc8945033daa69ea3f9340c69877aaa2c4e
KarolZbird <karolzieba202@gmail.com>
2026-06-13 19:27:00 +0200

    Dodanie mechanizmu nadpisywania najstarszej operacji po przekroczeniu limitu 10 zapisów w historii

--- memory.py
+++ memory.py
@@ -3,7 +3,7 @@ from operation_model import OperationModel
 import json
 
 class Memory:
-    def __init__(self, size: int):
+    def __init__(self, size: int = 10):
         self.__memory: list[OperationModel] = []
         self.__max_size: int = size
 
@@ -18,7 +18,7 @@ class Memory:
     def size(self) -> int:
         return len(self.__memory)
 
-    def add_op(self, op: OperationModel) -> None:
+    def addo_op(self, op: OperationModel) -> None:
         if len(self.__memory) >= self.__max_size:
             self.__memory.pop(0)
         self.__memory.append(op)
